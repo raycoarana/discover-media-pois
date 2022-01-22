f7a4d2a0519b91d31808c208cfa434683992920c
rayco <me@raycoarana.com>
2022-01-22 22:49:47 +0100

    Finish downloader and minor fix to generator

--- downloader.main.kts
+++ downloader.main.kts
@@ -2,39 +2,37 @@
 
 @file:DependsOn("commons-io:commons-io:2.11.0")
 @file:DependsOn("commons-codec:commons-codec:1.15")
-@file:DependsOn("org.apache.commons:commons-compress:1.21")
-@file:DependsOn("org.eclipse.jgit:org.eclipse.jgit:6.0.0.202111291000-r")
+@file:DependsOn("org.jsoup:jsoup:1.14.3")
 
 import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.io.FileUtils
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import java.io.File
 import java.net.URL
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import kotlin.system.exitProcess
 
-val releaseVersion = args[0]
-println("Release: $releaseVersion")
-exitProcess(-1)
+val username = args[0]
+val password = args[1]
 
-val items = mapOf(
-    PoiType.ES_ONLY to SimpleDownloader(
+val items = listOf(
+    SimpleDownloader(
         name = "ES Speedcams",
         url = "https://www.todo-poi.es/radar/GARMIN_RADARES/garminvelocidad 2xx-12xx-13xx-14xx-2xxx-3xxx y posteriores.zip",
         filename = "garminvelocidad 2xx-12xx-13xx-14xx-2xxx-3xxx y posteriores.zip"
     ),
-    PoiType.EU to SimpleDownloader(
+    SimpleDownloader(
         name = "Mercadona POIs",
         url = "https://www.todo-poi.es/comercio/GARMIN/Mercadona.zip",
         filename = "Mercadona.zip"
     ),
-    PoiType.EU to LufopDownloader()
+    LufopDownloader(username = username, password = password)
 )
 
 val baseDir = File("sources")
 
-val poiFiles = items.map { (type, downloader) ->
+val poiFiles = items.map { downloader ->
     println("Starting download of ${downloader.name}")
 
     val downloadedFile = downloader.download(baseDir)
@@ -42,17 +40,12 @@ val poiFiles = items.map { (type, downloader) ->
     println("Downloaded file at ${downloadedFile.absolutePath}")
 
     val newHash = downloadedFile.updateHashIfNew()
-    PoiFile(downloadedFile, newHash, type)
+    PoiFile(downloadedFile, newHash)
 }
 
 if (poiFiles.any { it.hash != null  }) {
-    println("Preparing for release v$releaseVersion")
-
-    TODO("Run generation")
-
-    // Generate new files calling poi-to-discover-media
-    // Generate release file
-    // Commit/Push new hash files (should do in another script?)
+    println("New POIs downloaded.")
+    exitProcess(0)
 } else {
     println("No new POIs or SpeedCams, aborting...")
     exitProcess(-1)
@@ -82,15 +75,9 @@ fun File.updateHashIfNew(): String? {
         ?.also { lastVersionHashFile.updateHash(it) }
 }
 
-enum class PoiType {
-    EU,
-    ES_ONLY,
-}
-
 data class PoiFile(
     val file: File,
-    val hash: String?,
-    val type: PoiType
+    val hash: String?
 )
 
 interface Downloader {
@@ -110,31 +97,53 @@ class SimpleDownloader(
     }
 }
 
-class LufopDownloader : Downloader {
+class LufopDownloader(
+    private val username: String,
+    private val password: String
+) : Downloader {
     override val name: String = "Lufop EU CSV"
 
-    val lufopCsvUrl = "https://lufop.net/wp-content/plugins/downloads-manager/upload/Lufop-Zones-de-danger-EU-CSV.zip"
-    val lufopCsvFilename = "Lufop-Zones-de-danger-EU-CSV.zip"
-
-// Login => https://forum.lufop.net/ucp.php?mode=login
-// Extract Cookies AND Login Form
-/*
-username: {{ secrets.LUFOP_USERNAME}}
-password: {{ secrets.LUFOP_PASSWORD}}
-redirect: ./ucp.php?mode=login
-creation_time: 1642629667
-form_token: 61d91110412184d28df01779857868f3fc4b8b03
-sid: 8cccfbaa138547b113df0292475160c5
-redirect: index.php
-login: Acceso
-*/
-
-// POST with cookies AND form to https://forum.lufop.net/ucp.php?mode=login
-// Extract cookies
-
-// GET https://lufop.net/wp-content/plugins/downloads-manager/upload/Lufop-Zones-de-danger-EU-CSV.zip
-
     override fun download(baseDir: File): File {
-        TODO("Not yet implemented")
+        val file = File(baseDir, "Lufop-Zones-de-danger-EU-CSV.zip")
+
+        val loginPage = Jsoup.connect("https://forum.lufop.net/ucp.php?mode=login")
+            .method(Connection.Method.GET)
+            .execute()
+
+        val loginForm = loginPage.parse().getElementById("login") ?: error("No login form found!")
+        val redirect = loginForm.getValueByName("redirect")
+        val creationTime = loginForm.getValueByName("creation_time")
+        val sid = loginForm.getValueByName("sid")
+        val formToken = loginForm.getValueByName("form_token")
+
+        val loginResult = Jsoup.connect("http://www.mikeportnoy.com/forum/login.aspx")
+            .data("username", username)
+            .data("password", password)
+            .data("redirect", redirect)
+            .data("creation_time", creationTime)
+            .data("form_token", formToken)
+            .data("sid", sid)
+            .cookies(loginPage.cookies())
+            .method(Connection.Method.POST)
+            .followRedirects(false)
+            .execute()
+
+        val zipFileResponse = Jsoup.connect("https://lufop.net/wp-content/plugins/downloads-manager/upload/Lufop-Zones-de-danger-EU-CSV.zip")
+            .cookies(loginResult.cookies())
+            .method(Connection.Method.GET)
+            .ignoreContentType(true)
+            .execute()
+
+        if (zipFileResponse.contentType() != "application/zip") {
+            error("Unexpected content type ${zipFileResponse.contentType()}")
+        }
+        zipFileResponse.bodyStream().use { bodyStream ->
+            FileUtils.copyInputStreamToFile(bodyStream, file)
+        }
+
+        return file
     }
+
+    private fun Element.getValueByName(name: String): String =
+        getElementsByAttributeValue("name", name).firstOrNull()?.`val`() ?: error("No $name element found or null!")
 }
